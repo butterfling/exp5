2312b940cf18c45ffb49397a411d300d2b1d2bee
Saahil Rathore <500097152@stu.upes.ac.in>
2024-04-24 10:37:41 +0530

    devops experiment 5

---
+++ Infrastructure1/test.py
@@ -0,0 +1,22 @@
+class Test:
+
+    def __init__(self, x, y):
+        self._x = 0
+
+    def get_x(self):
+        return self._x
+    
+    def set_x(self, value):
+        self._x = value
+    
+    def print_x(self):
+        print(self._x)
+
+    def print_string(self, y):
+        print(y)
+
+
+object = Test(1,2)
+
+object.print_string("Infra1")
+
